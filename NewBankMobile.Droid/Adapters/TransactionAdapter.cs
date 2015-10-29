00b823b73a519e4301ef435f685c140add5bb74e
Andres Pineda <pineda.andres@gmail.com>
2015-10-29 01:06:26 -0400

    Add empty activities and fragments

---
+++ NewBankMobile.Droid/Adapters/TransactionAdapter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Android.Widget;
+using Android.Views;
+
+namespace NewBankMobile.Droid.Adapters
+{
+    public class TransactionAdapter : BaseAdapter<Transaction>
+    {
+        public TransactionAdapter()
+        {
+            
+        }
+
+        #region implemented abstract members of BaseAdapter
+
+        public override long GetItemId(int position)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Count
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        #endregion
+
+        #region implemented abstract members of BaseAdapter
+
+        public override Transaction this[int index]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        #endregion
+    }
+}
+
