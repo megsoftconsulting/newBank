bd910844a38c548abcd6c0e11a828564b3fa901b
Andres Pineda <pineda.andres@gmail.com>
2015-10-29 00:05:29 -0400

    Add main entities

---
+++ NewBankMobile/Models/AccountOwner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NewBankMobile
+{
+    public class AccountOwner
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public AccountOwner()
+        {
+        }
+    }
+}
+
---
+++ NewBankMobile/Models/Transaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NewBankMobile
+{
+    public class Transaction
+    {
+        public Guid Id { get; set; }
+        public TransactionType TransactionType { get; set; }
+        public string AffectedAccount { get; set; }
+        public DateTime Date { get; set; }
+
+        public Transaction()
+        {
+        }
+    }
+}
+
