4f4c12fa347a58c828cfd5be1088b009f261e198
Andres Pineda <pineda.andres@gmail.com>
2015-10-30 11:58:27 -0400

    Fix csproj issue

---
+++ NewBankMobile/Repositories/LoginRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NewBankMobile.Interfaces;
+using System.Threading.Tasks;
+using NewBankMobile.Models;
+
+namespace NewBankMobile
+{
+    public class LoginRepository : ILoginRepository
+    {
+        public LoginRepository()
+        {
+        }
+
+        #region ILoginRepository implementation
+
+        public Task<User> LoginAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
+
