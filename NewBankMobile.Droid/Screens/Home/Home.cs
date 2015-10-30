7355b384788bc2d2b9ff9c4ee514399640219b27
Luis Nunez <luis@megsoftconsulting.com>
2015-10-29 21:09:36 -0400

    Implemented Home screen

---
+++ NewBankMobile.Droid/Screens/Home/Home.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V4.App;
+using Android.Support.V4.Widget;
+using Android;
+using Android.Views;
+using Android.Support.Design.Widget;
+using Android.Support.V7.Widget;
+using Android.Support.V7.App;
+using Android.Graphics;
+
+namespace NewBankMobile.Droid
+{
+	//No MVVM SO FAR
+	[Activity(Theme="@style/MyTheme.Base")]
+	public class Home : AppCompatActivity
+	{
+		DrawerLayout drawerLayout;
+
+		NavigationView navigationView;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			SetContentView (Resource.Layout.Home);
+
+			var toolbar = FindViewById<Toolbar>(Resource.Id.my_awesome_toolbar);
+
+//			toolbar.SetTitleTextColor(2232);
+
+			SetSupportActionBar (toolbar);
+
+			SupportActionBar.SetHomeAsUpIndicator (Resource.Drawable.ic_home_icono_burger);
+
+			SupportActionBar.SetDisplayHomeAsUpEnabled (true);
+
+			drawerLayout = FindViewById<DrawerLayout> (Resource.Id.drawer_layout);
+
+			navigationView = FindViewById<NavigationView> (Resource.Id.nav_view);
+
+			navigationView.NavigationItemSelected+= OnNavigationItemSelected;
+		}
+
+		void OnNavigationItemSelected (object sender, NavigationView.NavigationItemSelectedEventArgs e)
+		{
+			e.MenuItem.SetChecked (true);
+
+			drawerLayout.CloseDrawers ();
+		}
+
+		public override bool OnOptionsItemSelected (IMenuItem item) 
+		{
+			switch (item.ItemId) 
+			{
+			case Android.Resource.Id.Home:
+				drawerLayout.OpenDrawer (Android.Support.V4.View.GravityCompat.Start);
+				return true;
+			}
+			return base.OnOptionsItemSelected (item);
+		}
+	}
+}
+
--- NewBankMobile.Droid/Screens/Login/LoginScreen.cs
+++ NewBankMobile.Droid/Screens/Login/LoginScreen.cs
@@ -10,24 +10,51 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Android.Support.V7.App;
 
 namespace NewBankMobile.Droid
 {
-	[Activity (Label = "LoginScreen",MainLauncher = true)]			
-	public class LoginScreen : Activity
+	[Activity (Label = "LoginScreen",MainLauncher = true, Theme="@style/Theme.AppCompat")]			
+	public class LoginScreen : AppCompatActivity
 	{
+		Button _loginButton;
+
+		EditText _userName;
+
+		EditText _password;
+
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
 
 			SetContentView(Resource.Layout.LoginScreen);
 
+			SetViewReferences();
+
 			DecorateWindow();
+		}
+
+		void SetViewReferences ()
+		{
+			_loginButton = FindViewById<Button>(Resource.Id.loginButton);
+
+			_userName = FindViewById<EditText>(Resource.Id.userName);
+
+			_password = FindViewById<EditText>(Resource.Id.password);
+
+//			_loginButton.SetBinding
+
+			_loginButton.Click += OnLoginButtonClick;
+		}
+
+		void OnLoginButtonClick (object sender, EventArgs e)
+		{
+			StartActivity(typeof(Home));
 		}
 
 		void DecorateWindow ()
 		{
-			ActionBar.Hide();
+//			ActionBar.Hide();
 
 			Window.ClearFlags(WindowManagerFlags.Fullscreen);
 		}
