2ac5ac1dbf0bd797e2e59cff551d50194340d693
Claudio Sanchez <claudio@megsoftconsulting.com>
2015-10-29 22:53:18 -0400

    Missing files

---
+++ NewBankMobile/Interfaces/IAccountRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using NewBankMobile.Models;
+
+namespace NewBankMobile.Interfaces
+{
+    /// <summary>
+    /// </summary>
+    public interface IAccountRepository
+    {
+        // Task<Account> Add(Account account);
+        //Task<IList<Account>> ListAllByOwner(string ownerId);
+        // Task<Account> Update(Account account);
+        Task<List<Account>> GetByUserIdAsync(long id);
+        Task<Account> GetByIdAsync(long id);
+    }
+}
