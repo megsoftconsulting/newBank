8ef978af221db9903ccbb9c7a0123451190c5d23
Luis Ramirez <lyrc@outlook.com>
2015-11-12 01:37:14 -0400

    - Added Web Api project for manage App API - Reference Core to Api project for access to DTOs

---
+++ NewBankMobile.Api/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace NewBankMobile.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
